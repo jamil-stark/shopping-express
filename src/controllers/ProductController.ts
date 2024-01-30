4a1a10483db174a2323fc63b4ff04cfdf36339e6
Lutaaya Jamil <jamillutaaya0@gmail.com>
2024-01-30 14:37:47 +0300

    returned product with all it's data after creation

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { UploadProductDTO } from "../dto/request/uploadProduct.dto";
 import { AppDataSource } from "../data-source";
 import { Product } from "../entity/Product";
+import { GeneralDTO } from "../dto/response/general.tdo";
 
 export class ProductController{
     async createProduct(req: Request, res: Response){
@@ -20,7 +21,11 @@ export class ProductController{
         product.price = body.price
         try{
             await productRepository.save(product)
-            res.status(201).json({message: "Product created successfully!"})
+            const generalDTO: GeneralDTO = new GeneralDTO();
+            generalDTO.status = 201
+            generalDTO.message = "Product created successfully!"
+            generalDTO.data = product
+            res.status(201).json(generalDTO)
         }catch(error){
             console.error("Error during product creation:", error)
             res.status(500).json({message: error.message})
